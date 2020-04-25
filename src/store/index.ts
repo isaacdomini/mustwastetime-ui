d03a10a96d90da544df16624f38e494b5ed42d31
JohnSimerlink <johnsimerlink@gmail.com>
2020-04-25 17:33:14 -0400

    Count clicks of bubbles

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    bubblesClicked: 0,
+  },
+  mutations: {
+    clickBubble(state) {
+      state.bubblesClicked++;
+    }
+  },
   actions: {},
   modules: {}
 });
