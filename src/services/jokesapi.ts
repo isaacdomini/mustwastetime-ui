9ab7f77fe376c83a8b89856d116eecb95f8fb982
Isaac Domini <7013557+isaacdomini@users.noreply.github.com>
2021-03-24 09:16:37 -0400

    Use safer jokes

--- src/services/jokesapi.ts
+++ src/services/jokesapi.ts
@@ -3,7 +3,7 @@ import Joke from "@/model/joke";
 export default class JokesAPI {
   //TODO: move to contsants"
   public static API_URL =
-    "https://sv443.net/jokeapi/v2/joke/Miscellaneous?blacklistFlags=nsfw,sexist";
+    "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
   async getJoke(): Promise<Joke> {
     console.log("something is wrong");
     return axios
